e5e103c7c01be6f948ac0606dda177158a19d76b
Soulthym <thymi92@gmail.com>
2019-10-24 10:58:26 +0200

    che/a/cksum

--- flashserial.sh
+++ flashserial.sh
@@ -8,7 +8,7 @@ echo "compressing $src ($(du -h "$src" | awk '{print $1;}'))"
 cat "$src" | gzip | base64 > rbf.64.gz
 echo "compressed size: $(du -h rbf.64.gz | awk '{print $1;}')"
 md5=$(md5sum "$src" | awk '{print $1}')
-echo "sending the flashing script with md5 cheacksum"
+echo "sending the flashing script with md5 checksum"
 printf "\necho \"[ \\\$(md5sum /home/root/default.rbf | awk '{print \\\$1}') == %s ] &&\\ 
 echo 0 > /sys/class/fpga-bridge/fpga2hps/enable && echo 0 > /sys/class/fpga-bridge/hps2fpga/enable && echo 0 > /sys/class/fpga-bridge/lwhps2fpga/enable && dd if=/home/root/default.rbf of=/dev/fpga0 bs=1M
 echo 1 > /sys/class/fpga-bridge/fpga2hps/enable
